666ba7d61f6d7c8bd7f0409a2bc93924a2add5b2
Christian Schroeder de Witt <schroederdewitt@gmail.com>
2023-11-02 22:47:18 +0000

    fixed sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it -e WANDB_API_KEY=$WANDB_API_KEY --user $(id -u) -v $(pwd):/home/duser -it --gpus "device=$1" cleanrl ${@:2}
+docker run -it -e WANDB_API_KEY=$WANDB_API_KEY --user $(id -u) -v $(pwd):/home/duser -it --gpus "device=$1" torch_ngp ${@:2}
